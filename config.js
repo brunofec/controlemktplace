9f6924ed668295422cd86d82175dc68c9188b414
brunofec <brunofec@koerich.com.br>
2025-08-25 14:14:44 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// public/config.js
+window.ENV = {
+  SUPABASE_URL: 'https://zjdgqlqbclqarwobpgjp.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqZGdxbHFiY2xxYXJ3b2JwZ2pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMzk4MTYsImV4cCI6MjA3MTcxNTgxNn0.5MMcVPqSR3x-GNlJQQBtMy5PjnTZpn87Rgg6uEghoM8'
+};
